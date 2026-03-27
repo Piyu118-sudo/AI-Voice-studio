5c020f4dd7caee68cca8764cda0e6f18585089b2
Piyush prasad <piyushpalak04@gmail.com>
2026-03-27 14:18:31 +0530

    fix audio merge errors

--- src/app/lib/audio.ts
+++ src/app/lib/audio.ts
@@ -1,15 +1,16 @@
-
-export const mergeAudio = async (audioUrl: string[]) => {
+export const mergeAudio = async (audioUrls: string[]) => {
     const audioContext = new AudioContext();
 
     const buffers = await Promise.all(
-        audioUrls.map((url) => {
-            const res = await fetch(url),
+        audioUrls.map(async (url) => {
+            const res = await fetch(url);
             const arrayBuffer = await res.arrayBuffer();
             return await audioContext.decodeAudioData(arrayBuffer);
         })
     );
+
     const totalLength = buffers.reduce((sum, b) => sum + b.length, 0);
+
     const output = audioContext.createBuffer(
         1,
         totalLength,
@@ -18,14 +19,17 @@ export const mergeAudio = async (audioUrl: string[]) => {
 
     let offset = 0;
 
-    buffers.foreach((buffer) => {
+    buffers.forEach((buffer) => {
         output.getChannelData(0).set(buffer.getChannelData(0), offset);
         offset += buffer.length;
     });
+
     return output;
 };
+
+
 export const bufferToWav = (buffer: AudioBuffer) => {
     const channelData = buffer.getChannelData(0);
     const blob = new Blob([channelData], { type: "audio/wav" });
     return URL.createObjectURL(blob);
-}
+};
